3fdb7bbc59a6bfebea1070c3b9fdfffc2c9085b9
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-30 14:15:53 +0530

    Create initial ProductsPage component
    This component have element for product page title and product items

---
+++ src/components/pages/products-page/ProductsPage.jsx
@@ -0,0 +1,12 @@
+import ItemContainer from "../../item-container/ItemContainer";
+
+const ProductsPage = () => {
+  return (
+    <div>
+      <h2>Product</h2>
+      <ItemContainer />
+    </div>
+  );
+};
+
+export default ProductsPage;
