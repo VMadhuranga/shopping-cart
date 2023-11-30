2b1901dce79b874d53b45e868e7246018c84bd1e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-30 14:23:57 +0530

    Add browser routes
    Add routes for App, HomePage, ProductsPage and CartPage

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+import HomePage from "./components/pages/home-page/HomePage.jsx";
+import ProductsPage from "./components/pages/products-page/ProductsPage.jsx";
+import CartPage from "./components/pages/cart-page/CartPage.jsx";
 import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "products",
+        element: <ProductsPage />,
+      },
+      {
+        path: "cart",
+        element: <CartPage />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
