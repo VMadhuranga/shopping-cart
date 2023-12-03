23c8795b2031d19dc2525c73c251f1501b40926c
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-03 12:06:41 +0530

    Create ErrorPage component to handle bad route paths

---
+++ src/components/pages/error-page/ErrorPage.jsx
@@ -0,0 +1,21 @@
+import { Link, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div data-testid="ErrorPage">
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+      <p>
+        Go back to <Link to="/">Home</Link>
+      </p>
+    </div>
+  );
+};
+
+export default ErrorPage;
