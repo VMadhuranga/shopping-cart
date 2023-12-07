da6421494a21e311220fa9f88ec7f73b0c7ef024
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-07 08:16:55 +0530

    Add props to Item component
    Additional:
    - Add width and loading attributes to the img element
    - Add name attribute to the input element

--- src/components/item/Item.jsx
+++ src/components/item/Item.jsx
@@ -1,18 +1,24 @@
+import PropTypes from "prop-types";
 import Button from "../button/Button";
 
-const Item = () => {
+const Item = ({ imageUrl, itemName, itemPrice }) => {
   return (
     <div>
-      <img src="/" alt="/" />
-      <p>Item Name</p>
-      <p>Item Description</p>
-      <p>Item Price</p>
-      <input type="number" />
+      <img src={imageUrl} alt={itemName} width={200} loading="lazy" />
+      <p>{itemName}</p>
+      <p>{itemPrice}</p>
+      <input name="item-count" type="number" />
       <Button text={"-"} />
       <Button text={"+"} />
       <Button text={"Add to cart"} />
     </div>
   );
+};
+
+Item.propTypes = {
+  imageUrl: PropTypes.string,
+  itemName: PropTypes.string,
+  itemPrice: PropTypes.number,
 };
 
 export default Item;
