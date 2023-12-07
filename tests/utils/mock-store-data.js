7a52b8b4a33bee8bbed931f0b0875be0c7c8f6d8
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-07 08:18:11 +0530

    Create mock store data

---
+++ tests/utils/mock-store-data.js
@@ -0,0 +1,32 @@
+const mockStoreData = [
+  {
+    id: 1,
+    title: "T-Shirt 1",
+    price: 10,
+    image:
+      "https://images.unsplash.com/photo-1581655353564-df123a1eb820?q=80&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    title: "T-Shirt 2",
+    price: 15,
+    image:
+      "https://images.unsplash.com/photo-1618354691373-d851c5c3a990?w=200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjN8fHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+  {
+    id: 3,
+    title: "Trouser 1",
+    price: 20,
+    image:
+      "https://images.unsplash.com/photo-1624378439575-d8705ad7ae80?w=200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dHJvdXNlcnN8ZW58MHx8MHx8fDA%3D",
+  },
+  {
+    id: 4,
+    title: "Trouser 2",
+    price: 25,
+    image:
+      "https://images.unsplash.com/photo-1605518216938-7c31b7b14ad0?w=200&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cGFudHxlbnwwfHwwfHx8MA%3D%3D",
+  },
+];
+
+export default mockStoreData;
