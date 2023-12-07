7a4f29ea76c4909d5a11f69d8ce2da6bba92f6e7
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-07 08:24:40 +0530

    Render element according to values in data, error, loading

--- src/components/pages/products-page/ProductsPage.jsx
+++ src/components/pages/products-page/ProductsPage.jsx
@@ -1,10 +1,26 @@
-import ItemContainer from "../../item-container/ItemContainer";
+import { useOutletContext } from "react-router-dom";
+import Item from "../../item/Item";
 
 const ProductsPage = () => {
+  const { data, error, loading } = useOutletContext();
+
   return (
     <div data-testid={"ProductsPage"}>
       <h2>Product</h2>
-      <ItemContainer />
+      {loading && <p>Loading</p>}
+      {error && <p>Something went wrong</p>}
+      {data && (
+        <div data-testid="ItemContainer">
+          {data.map((product) => (
+            <Item
+              key={product.id}
+              imageUrl={product.image}
+              itemName={product.title}
+              itemPrice={product.price}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
