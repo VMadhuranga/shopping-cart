bedbba1ce1e8fbf3f28c0d4c00df43243451f515
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-09 13:41:23 +0530

    Update ProductPage component
    Get data, error, loading, cartData and setCartData from
    useOutletContext
    
    Create handleAddToStore function
    	This function update itemQuantity if item already in the cartData
      otherwise add the item to the cartData
    
    Create checkItemIsInTheCart function

--- src/components/pages/products-page/ProductsPage.jsx
+++ src/components/pages/products-page/ProductsPage.jsx
@@ -1,22 +1,69 @@
 import { useOutletContext } from "react-router-dom";
 import Item from "../../item/Item";
+import Button from "../../button/Button";
 
 const ProductsPage = () => {
-  const { data, error, loading } = useOutletContext();
+  const {
+    data: productsData,
+    error,
+    loading,
+    cartData,
+    setCartData,
+  } = useOutletContext();
+
+  function handleAddToCart(e) {
+    const itemId = Number(e.target.parentElement.dataset.itemId);
+    const itemData = productsData.find((product) => product.id === itemId);
+
+    // If item already in the cart update item item quantity
+    // Else add item to cart
+    if (checkItemIsInTheCart(cartData, itemData.id)) {
+      const updatedCartData = cartData.map((data) => {
+        if (data.id === itemData.id) {
+          data.itemQuantity += 1;
+          data.subTotalPrice = data.itemQuantity * data.price;
+        }
+
+        return data;
+      });
+
+      setCartData(updatedCartData);
+    } else {
+      const itemQuantity = 1;
+      const subTotalPrice = itemQuantity * itemData.price;
+
+      setCartData([
+        ...cartData,
+        {
+          ...itemData,
+          itemQuantity,
+          subTotalPrice,
+        },
+      ]);
+    }
+  }
+
+  function checkItemIsInTheCart(cartData, itemId) {
+    return cartData.some((data) => data.id === itemId);
+  }
 
   return (
     <div data-testid={"ProductsPage"}>
       <h2>Product</h2>
       {loading && <p>Loading</p>}
       {error && <p>Something went wrong</p>}
-      {data && (
+      {productsData && (
         <div data-testid="ItemContainer">
-          {data.map((product) => (
+          {productsData.map((product) => (
             <Item
               key={product.id}
+              itemId={product.id}
               imageUrl={product.image}
               itemName={product.title}
               itemPrice={product.price}
+              optionalElements={
+                <Button text={"Add to cart"} handleClick={handleAddToCart} />
+              }
             />
           ))}
         </div>
