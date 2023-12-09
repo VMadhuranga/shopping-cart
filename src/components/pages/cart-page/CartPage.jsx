0c40dc2d6a3a8275ad44d86c66f1aa6ca522cc29
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-09 13:42:38 +0530

    Update CartPage component
    Get cartData from useOutletContext
    Conditionally render cart page elements using cartData

--- src/components/pages/cart-page/CartPage.jsx
+++ src/components/pages/cart-page/CartPage.jsx
@@ -1,13 +1,44 @@
-import ItemContainer from "../../item-container/ItemContainer";
 import Button from "../../button/Button";
+import Item from "../../item/Item";
+import { useOutletContext } from "react-router-dom";
 
 const CartPage = () => {
+  const { cartData } = useOutletContext();
+
   return (
     <div data-testid="CartPage">
       <h2>Your Cart</h2>
-      <ItemContainer />
-      <p>Subtotal</p>
-      <Button text={"Checkout"} />
+      {cartData.length > 0 ? (
+        <>
+          <div data-testid="CartItemContainer">
+            {cartData.map((data) => (
+              <Item
+                key={data.id}
+                imageUrl={data.image}
+                itemName={data.title}
+                itemPrice={data.price}
+                subTotalPrice={data.subTotalPrice}
+                optionalElements={
+                  <>
+                    <input
+                      type="number"
+                      value={data.itemQuantity}
+                      name="quantity"
+                    />
+                    <Button text={"+"} />
+                    <Button text={"-"} />
+                    <Button text={"x"} />
+                  </>
+                }
+              />
+            ))}
+          </div>
+          <p>Total</p>
+          <Button text={"Checkout"} />
+        </>
+      ) : (
+        <p>Your cart is empty</p>
+      )}
     </div>
   );
 };
