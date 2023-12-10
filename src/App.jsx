eddaaf4af9f63ca7b6ee556a40dbf83f5f521d90
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-10 08:25:17 +0530

    Update App component
    Create cartCount, setCartCount state with initial value of 0
    Add effect to update cartCount when new item added or item quantity
    increases
    Remove NavigationBar component and add nav links inside header section
    Pass cartCount to the CartCounter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { Outlet } from "react-router-dom";
-import { useState } from "react";
-import NavigationBar from "./components/navigation-bar/NavigationBar";
+import { Link, Outlet } from "react-router-dom";
+import { useEffect, useState } from "react";
 import useFetchStoreData from "./utils/store-data";
+import CartCounter from "./components/cart-counter/CartCounter";
 
 const App = () => {
   const [cartData, setCartData] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
   const { data, error, loading } = useFetchStoreData();
+
+  useEffect(() => {
+    setCartCount(cartData.reduce((acc, data) => acc + data.itemQuantity, 0));
+  }, [cartData]);
 
   return (
     <>
       <header>
         <h1>Shopping Cart</h1>
-        <NavigationBar />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="products">Products</Link>
+            </li>
+            <li>
+              <Link to="cart">Cart</Link>
+              <CartCounter count={cartCount} />
+            </li>
+          </ul>
+        </nav>
       </header>
       <main>
         <Outlet context={{ data, error, loading, cartData, setCartData }} />
