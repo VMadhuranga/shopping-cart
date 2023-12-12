84918b321ddfead4b2d436d70674ba2c711f2253
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-12 08:35:43 +0530

    Update HomePage component
    Add homePage and pageTransition classes to div element
    Create p element with dummy content
    Add image to home page

--- src/components/pages/home-page/HomePage.jsx
+++ src/components/pages/home-page/HomePage.jsx
@@ -1,5 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import Button from "../../button/Button";
+import styles from "./HomePage.module.css";
+import { pageTransition } from "../../../App.module.css";
 
 const HomePage = () => {
   const navigate = useNavigate();
@@ -9,9 +11,20 @@ const HomePage = () => {
   }
 
   return (
-    <div data-testid="HomePage">
-      <h2>Calling all fashion lovers</h2>
-      <Button text={"Shop Now"} handleClick={navigateToProducts} />
+    <div
+      className={`${styles.homePage} ${pageTransition}`}
+      data-testid="HomePage"
+    >
+      <div>
+        <h2>Calling all fashion lovers</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo
+          explicabo fugiat numquam dignissimos accusamus quasi ipsa rem nam
+          minima unde.
+        </p>
+        <Button text={"Shop Now"} handleClick={navigateToProducts} />
+      </div>
+      <img src="./src/assets/images/home-page-bg.jpg" alt="store front" />
     </div>
   );
 };
