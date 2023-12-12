ecaeca645ff948577ab62c59e8df3d629dd4eb27
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-12-12 12:50:42 +0530

    Add errorPage class to div element

--- src/components/pages/error-page/ErrorPage.jsx
+++ src/components/pages/error-page/ErrorPage.jsx
@@ -1,11 +1,12 @@
 import { Link, useRouteError } from "react-router-dom";
+import { errorPage } from "./ErrorPage.module.css";
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div data-testid="ErrorPage">
+    <div className={errorPage} data-testid="ErrorPage">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
